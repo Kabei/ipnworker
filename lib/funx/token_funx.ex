e1e10b208a8d6217ce794a24eaa69c31f0c6a7be
kabei <kambei.sapote@gmail.com>
2023-09-26 18:42:28 -0400

    dets_plux, token supply

--- lib/funx/token_funx.ex
+++ lib/funx/token_funx.ex
@@ -4,8 +4,8 @@ defmodule Ippan.Funx.Token do
   require BalanceStore
 
   @type result :: Ippan.Request.result()
-  @token Application.compile_env(:ipnworker, :token)
-  @max_tokens Application.compile_env(:ipnworker, :max_tokens)
+  @token Application.compile_env(:ipncore, :token)
+  @max_tokens Application.compile_env(:ipncore, :max_tokens)
   @table_name "assets.token"
 
   def new(
@@ -84,7 +84,9 @@ defmodule Ippan.Funx.Token do
   end
 
   def delete(%{id: account_id, conn: conn, stmts: stmts}, id) do
-    if TokenSupply.get(id) == 0 do
+    tx = DetsPlux.tx(:supply)
+
+    if TokenSupply.get(tx, id) == 0 do
       SqliteStore.step(conn, stmts, "delete_token", [id, account_id])
     end
   end
