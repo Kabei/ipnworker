c64cdcc46e70ef1a15b82f997e395d921adece6c
kabei <kambei.sapote@gmail.com>
2023-10-21 16:06:11 -0400

    fix funcs

--- test/request_test.exs
+++ test/request_test.exs
@@ -3,11 +3,11 @@ defmdoule Request do
   doctest Ipncore
   import Builder, only: [post: 2]
 
-  test "" do
+  test "send requests" do
     hostname = "visurpay.com"
     {c1, c2} = Builder.test()
     {:ok, c2} = Builder.wallet_sub(c2, vid) |> post(hostname)
 
-
+    {:ok, c2} = Builder.coin_send(c1, c2.address, "IPN", 50) |> post(hostname)
   end
 end
