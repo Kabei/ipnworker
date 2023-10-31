d91aec3ab679d6a6676e8d968a95e0d5a1783ca2
kabei <kambei.sapote@gmail.com>
2023-10-31 19:09:08 -0400

    update

--- lib/p2p/cluster/cluster_nodes.ex
+++ lib/p2p/cluster/cluster_nodes.ex
@@ -21,46 +21,7 @@ defmodule Ippan.ClusterNodes do
     sup: Ippan.ClusterSup
 
   def on_init(_) do
-    load_nodes()
     connect_to_miner()
-  end
-
-  defp load_nodes do
-    db_ref = :persistent_term.get(:net_conn)
-
-    if Node.total() == 0 do
-      nodes = System.get_env("NODES")
-
-      if is_nil(nodes) do
-        IO.puts(IO.ANSI.red() <> "ERROR: variable NODES is missing" <> IO.ANSI.reset())
-        System.halt(1)
-      end
-
-      pk = :persistent_term.get(:pubkey)
-      net_pk = :persistent_term.get(:net_pubkey)
-      default_port = Application.get_env(@app, :cluster)[:port]
-
-      # registry cluster nodes
-      String.split(nodes, ",", trim: true)
-      |> Enum.reduce([], fn x, acc ->
-        acc ++ [String.split(x, "@", parts: 2)]
-      end)
-      |> Enum.each(fn [name_id, hostname] ->
-        data =
-          %Node{
-            id: name_id,
-            hostname: hostname,
-            port: default_port,
-            pubkey: pk,
-            net_pubkey: net_pk
-          }
-          |> Node.to_list()
-
-        Node.insert(data)
-      end)
-
-      Sqlite.sync(db_ref)
-    end
   end
 
   defp connect_to_miner do
--- lib/sql/network.stmt.sql
+++ lib/sql/network.stmt.sql
@@ -1,5 +1,5 @@
 --name: insert_node
-INSERT INTO nodes VALUES(?1,?2,?3,?4,?5,?6,?7);
+INSERT INTO nodes VALUES(?1,?2,?3,?4,?5,?6,?7,?8,?9);
 
 --name: get_node
 SELECT * FROM nodes WHERE id=? LIMIT 1;
