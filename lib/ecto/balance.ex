7493ff7dc888c4de214da6bef4d68f422fdbbb99
kabei <kambei.sapote@gmail.com>
2023-11-20 23:49:57 -0400

    balance filter token

--- lib/ecto/balance.ex
+++ lib/ecto/balance.ex
@@ -1,6 +1,6 @@
 defmodule Ippan.Ecto.Balance do
   use Ecto.Schema
-  import Ecto.Query, only: [from: 2, order_by: 3, select: 3]
+  import Ecto.Query, only: [from: 2, order_by: 3, select: 3, where: 3]
   alias Ipnworker.Repo
   alias Ippan.Token
   require Ippan.Token
@@ -26,11 +26,18 @@ defmodule Ippan.Ecto.Balance do
     from(b in Balance, where: b.id == ^id)
     |> filter_offset(params)
     |> filter_limit(params)
+    |> filter_token(params)
     |> filter_select()
     |> sort(params)
     |> Repo.all()
     |> data(params)
   end
+
+  defp filter_token(query, %{"token" => token}) do
+    where(query, [x], x.token == ^token)
+  end
+
+  defp filter_token(query, _), do: query
 
   defp filter_select(query) do
     select(query, [x], map(x, @select))
@@ -38,9 +45,6 @@ defmodule Ippan.Ecto.Balance do
 
   defp sort(query, %{"sort" => "most_value"}), do: order_by(query, [x], desc: x.balance)
   defp sort(query, _), do: order_by(query, [x], desc: x.balance)
-
-  defp data([], %{"zero" => "0"}) do
-  end
 
   defp data(results, %{"extra" => _}) do
     db_ref = :persistent_term.get(:main_conn)
