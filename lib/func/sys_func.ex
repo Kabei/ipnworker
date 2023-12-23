eb5b05f521734a9de063e05aa34d592415f5a86e
kabei <kambei.sapote@gmail.com>
2023-12-23 19:45:37 -0400

    none

--- lib/func/sys_func.ex
+++ lib/func/sys_func.ex
@@ -1,6 +1,7 @@
 defmodule Ippan.Func.Sys do
   def upgrade(%{id: account_id}, %{"git" => _git}, _target) do
     cond do
+
       EnvStore.owner() != account_id ->
         raise IppanError, "Unauthorized"
 
