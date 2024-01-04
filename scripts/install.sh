ed8623bc0adefe1bc81122849dbb6a35edec7eca
kabei <kambei.sapote@gmail.com>
2024-01-04 16:23:03 -0400

    install

---
+++ scripts/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+export MIX_ENV=prod
+export PATH="/root/.cargo/bin:${PATH}"
+
+apt update -y
+apt install erlang elixir curl git cmake zip unzip -y
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+git clone --branch dev https://kabei@github.com/kabei/ipnworker.git
+
+cd ipnworker
+
+mix local.hex --force
+mix deps.get
+mix local.rebar --force
+mix compile
+
+cp ../env_file ./
+
