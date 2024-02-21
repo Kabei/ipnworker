e4ebeb2a569fac9d561f5f7ec38bf5362996adfd
kambei <kambei.sapote@gmail.com>
2024-02-21 19:34:29 -0400

    update

--- lib/structs/account.ex
+++ lib/structs/account.ex
@@ -1,13 +1,15 @@
 defmodule Ippan.Account do
   @behaviour Ippan.Struct
   @type t :: %__MODULE__{
-          id: String.t(),
-          pubkey: binary,
-          validator: non_neg_integer(),
-          sig_type: non_neg_integer()
-        }
+    id: String.t(),
+    pubkey: binary,
+    vid: non_neg_integer(),
+    fa: non_neg_integer(),
+    fb: non_neg_integer(),
+    sig_type: non_neg_integer()
+  }
 
-  defstruct [:id, :pubkey, :validator, :sig_type]
+  defstruct [:id, :fa, :fb, :pubkey, :vid, :sig_type]
 
   @impl true
   def to_list(x) do
