0bfe7044582c0b68b59004ee12585296457fe736
kabei <kambei.sapote@gmail.com>
2024-03-03 01:25:03 -0400

    debug

--- lib/p2p/cluster/cluster_nodes.ex
+++ lib/p2p/cluster/cluster_nodes.ex
@@ -116,7 +116,7 @@ defmodule Ippan.ClusterNodes do
   Create a new round. Received from a IPNCORE
   """
   def handle_message("round.new", msg_round, %{hostname: hostname} = _state) do
-    # IO.inspect("handle 1")
+    IO.inspect("round.new")
     round = MapUtil.to_atoms(msg_round)
     GenServer.cast(RoundBuilder, {:build, round, hostname, true})
   end
