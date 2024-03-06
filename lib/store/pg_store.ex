85f5459d8dddafd2ab83059f562ad8be9a227fbd
kabei <kambei.sapote@gmail.com>
2024-03-05 21:18:09 -0400

    update

--- lib/store/pg_store.ex
+++ lib/store/pg_store.ex
@@ -110,7 +110,7 @@ defmodule PgStore do
   def insert_round(conn, params) do
     Postgrex.query(
       conn,
-      query_parse("EXECUTE insert_round($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)", params),
+      query_parse("EXECUTE insert_round($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)", params),
       []
     )
   end
