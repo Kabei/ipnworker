17785ed820241bd668e90a691c6d11d2ded150cd
kabei <kambei.sapote@gmail.com>
2024-03-31 13:00:09 -0400

    update

--- lib/func/sys_func.ex
+++ lib/func/sys_func.ex
@@ -1,8 +1,16 @@
 defmodule Ippan.Func.Sys do
-  def upgrade(%{id: account_id}, _commands, _target) do
+  @dev_mode Mix.env() == :dev
+
+  if @dev_mode do
+    def upgrade(%{id: account_id}, %{"reset" => "reset_data"}, _target) do
+      if EnvStore.owner() != account_id, do: raise(IppanError, "Unauthorized")
+    end
+  end
+
+  def upgrade(%{id: account_id}, %{"git" => _git}, _target) do
     cond do
       EnvStore.owner() != account_id ->
-        raise IppanError, "Unauthorized "
+        raise IppanError, "Unauthorized"
 
       true ->
         :ok
