07019a9094ba8d5b735e8cefa61fb19efd2f1c7d
kabei <kambei.sapote@gmail.com>
2024-03-31 12:26:08 -0400

    service.stream interval

--- lib/funx/service_funx.ex
+++ lib/funx/service_funx.ex
@@ -112,7 +112,7 @@ defmodule Ippan.Funx.Service do
         current_interval = div(round_id, every)
 
         if (max_spent == 0 or
-              spent + amount <= max_spent) and
+              spent + amount <= max_spent) or
              current_interval >= interval do
           BalanceStore.pay payer, token_id, amount do
             if current_interval == interval do
