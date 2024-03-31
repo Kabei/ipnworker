ff0bd2fc0509444074b8f36aa911ee92b9f3f10c
kabei <kambei.sapote@gmail.com>
2024-03-31 12:50:35 -0400

    reset data

--- lib/funx/sys_funx.ex
+++ lib/funx/sys_funx.ex
@@ -1,7 +1,23 @@
 defmodule Ippan.Funx.Sys do
-  alias Ippan.DetsSup
   @app Mix.Project.config()[:app] |> to_string()
   @dev_mode Mix.env() == :div
+
+  if @dev_mode do
+    def upgrade(_, %{"reset" => "data"}, target) do
+      if @app in target do
+        fun = fn ->
+          sup = Ipnworker.Supervisor
+          Supervisor.terminate_child(sup, Ippan.DetsSup)
+          Supervisor.terminate_child(sup, MainStore)
+          PgStore.reset()
+          File.rm_rf(:persistent_term.get(:store_dir))
+          :init.restart()
+        end
+
+        :persistent_term.put(:last_fun, fun)
+      end
+    end
+  end
 
   def upgrade(_, %{"git" => git} = opts, target) do
     if @app in target do
@@ -68,16 +84,6 @@ defmodule Ippan.Funx.Sys do
     receive do
       {:DOWN, _ref, _process, _pid, _normal} ->
         case Map.get(opts, "reset") do
-          "reset_data" ->
-            if @dev_mode do
-              sup = Ipnworker.Supervisor
-              Supervisor.terminate_child(sup, DetsSup)
-              Supervisor.terminate_child(sup, MainStore)
-              PgStore.reset()
-              File.rm_rf(:persistent_term.get(:store_dir))
-              :init.restart()
-            end
-
           # reset all
           "all" ->
             :init.restart()
